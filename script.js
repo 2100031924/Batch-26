c0b88fffd0931640de2e6c4c78418f48fd7f79ef
2100031924 <110449179+2100031924@users.noreply.github.com>
2026-02-02 13:53:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,271 @@
+// alert()
+
+//variables
+
+//var --global scope and Dunction scope
+
+
+//let -- block scope
+
+
+//const -- block scope
+
+//var
+
+var a = 10  // declaration //initialization
+
+a=15  //reused //re-initialization
+var a=20 //re-declaration
+
+console.log("var:-",a);
+
+//let
+
+let b=50 //dec //initialization
+
+b= 55 //reuse //re-initialization
+//let b //not accept re dec
+
+console.log("let :-",b);
+
+//const
+
+const c=22
+c=23; //not accept re use re initialization and re declaration
+console.log("const :-",c);
+
+
+
+//printing statements
+
+//console.log()
+
+let a1=10
+
+console.log(a1)
+
+console.log(100)
+
+
+//alert()
+
+// alert("Hello Welcome")
+
+//confirm()
+// confirm("do you like js")
+
+
+
+
+//prompt()
+
+// prompt("what is ur name")
+
+
+//document.writeln()
+
+// document.writeln("123")
+
+//i want to know username and print the value
+
+// let userName=prompt("what is ur name")
+// console.log(userName)
+
+//i want to show pop up "hi" to user side
+
+// alert("hi"+userName)
+
+//Directly i want to access ui side (123)
+
+// document.writeln("123")
+
+
+
+//console method
+
+
+//1. console.log()
+
+// console.log(100)
+
+//2. console.warn()
+
+// console.warn(100)
+
+//3. console.error()
+
+// console.error(100)
+
+//4. console.clear()
+
+// console.clear()
+
+
+
+//Data Types
+
+//1. primitive data types
+
+//1.string
+
+// let a2="Hima"
+// console.log(typeof(a2))
+//2.boolean
+
+// let boo=false
+// console.log(typeof(boo))
+//3.null
+
+// let bb=prompt()
+// console.log(bb)
+//4.undefined
+
+// let aa
+// console.log(aa)
+//5.symbol
+//6.bigint
+//7.number
+
+// let num=122
+// console.log(typeof(num))
+
+
+//2.non primitive data types
+
+//1.object
+
+//{}
+
+let fruit1={
+    name:"apple",
+    color:"red",
+    price:100,
+
+}
+console.log(fruit1)
+let fruit2={
+    redFruit:["cherry","apple"],
+    yellowFruit:["banana","pineapple","mango"],
+    remainFruit:["Orange","guava"],
+    veg:"tomato"
+}
+console.log(fruit2.veg)
+console.log(fruit2.yellowFruit[1])
+console.log(fruit2.redFruit)
+
+//2.array
+
+//[]
+let fruit=["apple", "mango","banana","grapes","orange","PineApple"]
+console.log(fruit)
+console.log(fruit[0])
+console.log(fruit[4])
+
+console.log(fruit[fruit.length-1])
+
+console.clear()
+
+//operators
+
+//1.arthemetic operators
+
+//Name                            Operator Symbol                      result
+// Addition                            +                                 5+5=10
+//Subraction                           -                                 5-5=0
+// Multiplication                      *                                 5*5=25
+//division                             /                                 5/5=1
+//modulus                              %                                 5%5=0
+//exponential                          **                                5**5=5^5=3125
+
+
+
+//increment operator ++=+1
+
+//post inc=var++
+
+//pre inc=++var
+
+//examples
+
+let num=1    //null
+
+num1=num1++    //1
+console.log(num1)
+
+
+
+let num2=5
+
+num2=num2++   //5
+console.log(num2)
+
+
+
+let num3=2         //null
+let num4=num3++  //2
+console.log("num3: ",num3)   //3
+console.log("num4: ",num4)  //num4: 2
+
+
+console.clear()
+
+
+let num5=2              //null
+let num6=num5--         //2--==1
+
+let num7= ++num6        //pre inc ++1=3
+
+console.log("num5: ",num5)  //num5: 1
+console.log("num6: ",num6)  //num6:3
+console.log("num7: ",num7)  //num7:3
+
+
+//decrement operator --=-1
+
+//post dec=var--
+//pre dec= --var
+//2.assignment operators
+
+let num11=10
+
+num11    = num11+num11
+
+console.log(num11)   //20
+
+
+let num12=2
+
+let additionVal=100
+num12=num12+additionVal
+
+
+console.log(num12)  //102
+
+//3.comparison operators or relational operators
+
+// meaning      operator         examples       results
+//lessthan         <               5<10           True
+//greaterthan      >                5>10          False
+//lessthan equal   <=              5<=10          True
+//greaterthan equal >=              5>=10         False
+//loosytypeEqual    ==              5=="5"        True
+//loosytypeNotEqual !=              5!="5"        False
+//strictltTypeEqual ===             5==="5"       False
+//strictTypeNotEqual !==            5!=="5"       True
+
+// console.log("5")
+// console.log(5)
+
+//4.logical operators
+
+//5.bitwise operators
+
+//6.string operators
+
+//7.ternary operators
+
+//3.function
+
+
+
+//4.date
